55cbbfb2827a096103327b3e9e7ff6d943cb8501
simonj1704 <simo915m@stud.kea.dk>
2023-05-10 12:09:38 +0200

    Copied index from Oenskeskyen

--- src/main/java/com/example/bilabonnement/controller/Controller.java
+++ src/main/java/com/example/bilabonnement/controller/Controller.java
@@ -2,10 +2,15 @@ package com.example.bilabonnement.controller;
 
 import com.example.bilabonnement.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-
     @Autowired
     Service service;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
