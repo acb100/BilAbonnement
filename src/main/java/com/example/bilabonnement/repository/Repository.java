5fc858a0069ba16004dbb912bf88862c2ecd2ef1
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-05-10 15:46:01 +0200

    added addRentalContract to repo and service

--- src/main/java/com/example/bilabonnement/repository/Repository.java
+++ src/main/java/com/example/bilabonnement/repository/Repository.java
@@ -1,5 +1,6 @@
 package com.example.bilabonnement.repository;
 
+import com.example.bilabonnement.model.RentalContract;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -8,4 +9,12 @@ public class Repository {
 
     @Autowired
     JdbcTemplate template;
+    public void addRentalContract(RentalContract rentalContract){
+    String sql = "INSERT INTO rental_contract (rental_contract_id, start_date, end_date, ongoing, employee_id," +
+             "subscription_id, customer_id, car_id, damage_report_id) VALUES(?,?,?,?,?,?,?,?,?)";
+    template.update(sql, rentalContract.getRentalContractId(), rentalContract.getStartDate(), rentalContract.getEndDate(),
+            true, rentalContract.getEmployeeId(), rentalContract.getSubscriptionId(), rentalContract.getCustomerId(),
+            rentalContract.getCarId(), rentalContract.getDamageReportId());
+
+    }
 }
