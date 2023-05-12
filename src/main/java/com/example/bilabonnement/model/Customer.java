fe949abaeba61687b27659d31902dd9e15a22bda
simonj1704 <113358970+simonj1704@users.noreply.github.com>
2023-05-13 01:18:14 +0200

    Merged files

--- src/main/java/com/example/bilabonnement/model/Customer.java
+++ src/main/java/com/example/bilabonnement/model/Customer.java
@@ -1,87 +1,27 @@
 package com.example.bilabonnement.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @NoArgsConstructor @ToString
 public class Customer {
-    private int customerId;
-    private String customerName;
-    private String customerAddress;
-    private String customerEmail;
-    private String customerPhone;
-    private int driverLicenseNumber;
-    private int cprNumber;
-    private int employeeId;
+    private int customer_id;
+    private String customer_name;
+    private String address;
+    private String email;
+    private String phone_number;
+    private int driver_license_number;
+    private int cpr_number;
 
-    public Customer(int customerId, String customerName, String customerAddress, String customerEmail, String customerPhone, int driverLicenseNumber, int cprNumber, int employeeId) {
-        this.customerId = customerId;
-        this.customerName = customerName;
-        this.customerAddress = customerAddress;
-        this.customerEmail = customerEmail;
-        this.customerPhone = customerPhone;
-        this.driverLicenseNumber = driverLicenseNumber;
-        this.cprNumber = cprNumber;
-        this.employeeId = employeeId;
-    }
-
-    public int getCustomerId() {
-        return this.customerId;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getCustomerName() {
-        return this.customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getCustomerAddress() {
-        return this.customerAddress;
-    }
-
-    public void setCustomerAddress(String customerAddress) {
-        this.customerAddress = customerAddress;
-    }
-
-    public String getCustomerEmail() {
-        return this.customerEmail;
-    }
-
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
-
-    public String getCustomerPhone() {
-        return this.customerPhone;
-    }
-
-    public void setCustomerPhone(String customerPhone) {
-        this.customerPhone = customerPhone;
-    }
-
-    public int getDriverLicenseNumber() {
-        return this.driverLicenseNumber;
-    }
-
-    public void setDriverLicenseNumber(int driverLicenseNumber) {
-        this.driverLicenseNumber = driverLicenseNumber;
-    }
-
-    public int getCprNumber() {
-        return this.cprNumber;
-    }
-
-    public void setCprNumber(int cprNumber) {
-        this.cprNumber = cprNumber;
-    }
-
-    public int getEmployeeId() {
-        return this.employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
+    public Customer(int customer_id, String customer_name, String address, String email, String phone_number, int driver_license_number, int cpr_number, int employeeId) {
+        this.customer_id = customer_id;
+        this.customer_name = customer_name;
+        this.address = address;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.driver_license_number = driver_license_number;
+        this.cpr_number = cpr_number;
     }
 }
--- src/main/java/com/example/bilabonnement/model/DamageReport.java
+++ src/main/java/com/example/bilabonnement/model/DamageReport.java
@@ -1,37 +1,20 @@
 package com.example.bilabonnement.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
 public class DamageReport {
-    private int damageReportId;
-    private String damageReportContent;
-    private int employeeId;
+    private int damage_report_id;
+    private String damage_report_content;
+    private int employee_id;
 
-    public DamageReport(int damageReportId, String damageReportContent, int employeeId) {
-        this.damageReportId = damageReportId;
-        this.damageReportContent = damageReportContent;
-        this.employeeId = employeeId;
+    public DamageReport(int damage_report_id, String damage_report_content, int employee_id) {
+        this.damage_report_id = damage_report_id;
+        this.damage_report_content = damage_report_content;
+        this.employee_id = employee_id;
     }
 
-    public int getDamageReportId() {
-        return this.damageReportId;
-    }
 
-    public void setDamageReportId(int damageReportId) {
-        this.damageReportId = damageReportId;
-    }
-
-    public String getDamageReportContent() {
-        return this.damageReportContent;
-    }
-
-    public void setDamageReportContent(String damageReportContent) {
-        this.damageReportContent = damageReportContent;
-    }
-
-    public int getEmployeeId() {
-        return this.employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
 }
